f9f62dc1e942ea2e649fe2b2f46f28d63322f931
MESTRE-Andrea <andrea.mestre@edu.univ-fcomte.fr>
2025-10-09 09:35:48 +0200

    test

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -5,7 +5,8 @@ var path='';
 if (import.meta.env.MODE === 'development') {
     path = 'http://127.0.0.1:8090'; // PocketBase port par défaut
 } else {
-    path = 'http://eval.andrea-mestre.eu:8090'; 
+    // En production, PocketBase tourne sur le même serveur
+    path = 'http://127.0.0.1:8090'; 
 }
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
