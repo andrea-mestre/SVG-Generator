223212049cd34bf28307f9da3f2c3bb2d57501ad
MESTRE-Andrea <andrea.mestre@edu.univ-fcomte.fr>
2025-10-09 09:41:22 +0200

    fin tp

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -3,9 +3,8 @@ import type { TypedPocketBase } from "./pocketbase-types";
 var path='';
 
 if (import.meta.env.MODE === 'development') {
-    path = 'http://127.0.0.1:8090'; // PocketBase port par défaut
+    path = 'http://127.0.0.1:8090';
 } else {
-    // En production, PocketBase tourne sur le même serveur
     path = 'http://127.0.0.1:8090'; 
 }
 const pb = new PocketBase(path) as TypedPocketBase;
